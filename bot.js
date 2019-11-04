16d99ad498dc2e050af241722b2e91c0291cd900
Red <b.darras854@laposte.net>
2019-11-04 17:33:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -497,7 +497,7 @@ bot.on('message', message => {
             .setFooter("『SAO Community [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
             .setColor(3447003)
             .addField("Les espadons :", "Ce sont des épées maniées à deux mains pouvant mesurer entre 1m et 2m.\n\n" +
-                "Elles sont à deix mains !\n\n" +
+                "Elles sont à deux mains !\n\n" +
                 "Elles donnent généralement un bonus en Combo de +2.\n\n" +
                 "Elles vous donnent accès à la compétence suivante : \n" +
                 "`=Coup tournoyant 1 : [Votre attaque]`\n" +
@@ -510,17 +510,17 @@ bot.on('message', message => {
 });
 
 bot.on('message', message => {
-    if (message.content === (prefix) + "Les épées") {
+    if (message.content === (prefix) + "Les dagues") {
         const embed = new Discord.RichEmbed()
             .setAuthor(message.author.username, message.author.avatarURL)
             .setFooter("『SAO Community [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
             .setColor(3447003)
-            .addField("Les épées :", "Ce sont des armes blanches à double tranchant, composées d'une lame droite en métal, d'une poignée et parfois d'une garde.\n\n" +
+            .addField("Les dagues :", "Ce sont des armes blanches courte à simple ou double tranchant.\n\n" +
                 "Elles sont à une main !\n\n" +
-                "Elles donnent généralement un bonus en Combo de +1.\n\n" +
+                "Elles donnent généralement un bonus en Coup létal de +1.\n\n" +
                 "Elles vous donnent accès à la compétence suivante : \n" +
-                "`=Coup glissé : [Votre attaque]`")
-            .setImage("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Trp-Sword-14226124129-v06.png/800px-Trp-Sword-14226124129-v06.png")
+                "`=Coup sanglant : [Votre attaque]`")
+            .setImage("https://www.le-couteau.com/3648-thickbox_default/dague-muela-sanglier-24cm-inox.jpg")
             .setTimestamp()
         message.channel.send({ embed })
     }
