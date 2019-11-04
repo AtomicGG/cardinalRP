8bbbd24b83c010edb6e3044b1f00fcfe3324568f
Red <b.darras854@laposte.net>
2019-11-04 18:13:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -595,17 +595,17 @@ bot.on('message', message => {
 });
 
 bot.on('message', message => {
-    if (message.content === (prefix) + "Les épées") {
+    if (message.content === (prefix) + "Les rapières") {
         const embed = new Discord.RichEmbed()
             .setAuthor(message.author.username, message.author.avatarURL)
             .setFooter("『SAO Community [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
             .setColor(3447003)
-            .addField("Les épées :", "Ce sont des armes blanches à double tranchant, composées d'une lame droite en métal, d'une poignée et parfois d'une garde.\n\n" +
+            .addField("Les rapières :", "Ce sont des épées longues et fines, à la garde élaborée et la lame flexible.\n\n" +
                 "Elles sont à une main !\n\n" +
-                "Elles donnent généralement un bonus en Combo de +1.\n\n" +
+                "Elles donnent généralement un bonus en Coup d'estoc de +1.\n\n" +
                 "Elles vous donnent accès à la compétence suivante : \n" +
-                "`=Coup glissé : [Votre attaque]`")
-            .setImage("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Trp-Sword-14226124129-v06.png/800px-Trp-Sword-14226124129-v06.png")
+                "`=Désarmement : [L'attaque adverse]`")
+            .setImage("https://media2.faitsdarmes.com/513-thickbox_default/rapiere-regenyei.jpg")
             .setTimestamp()
         message.channel.send({ embed })
     }
