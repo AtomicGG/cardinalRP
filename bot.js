a7f00a270f5b0e624d0dd3f099a51ac4fea50adc
Red <b.darras854@laposte.net>
2019-11-04 15:46:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1527,7 +1527,17 @@ bot.on('message', message => {
                 "Niveau 28 : Vous gagnez 4.5 points d'armure et 30 HP et 3 points de dégât !\n\n" +
                 "Niveau 29 : Vous gagnez 4.5 points d'armure et 30 HP et 3 points de dégât !\n\n" +
                 "Niveau 30 : Vous gagnez 4.5 points d'armure et 35 HP et 6 points de dégât !")
-            .setImage("https://i.pinimg.com/originals/18/fb/4b/18fb4b82ad92387d26413f1ef3518d96.png")
+                .addBlankField(true)
+                .addField("Les bonus des niveaux pour Chevalier de 31 à 40 :", "Niveau 31 : Vous gagnez 4.5 points d’armure et 35 HP et 6 points de dégât ! \n\n" +
+                    "Niveau 32 : Vous gagnez 4.5 points d’armure et 35 HP et 6 points de dégât ! \n\n" +
+                    "Niveau 33 : Vous gagnez 4.5 points d’armure et 35 HP et 6 points de dégât ! \n\n" +
+                    "Niveau 34 : Vous gagnez 4.5 points d’armure et 35 HP et 6 points de dégât !\n\n" +
+                    "Niveau 35 : Vous gagnez 4.5 points d'armure et 35 HP et 6 points de dégât !\n\n" +
+                    "Niveau 36 : Vous gagnez 4.5 points d'armure et 35 HP et 6 points de dégât !\n\n" +
+                    "Niveau 37 : Vous gagnez 6 points d'armure et 45 HP et 6 points de dégât !\n\n" +
+                    "Niveau 38 : Vous gagnez 6 points d'armure et 45 HP et 6 points de dégât !\n\n" +
+                    "Niveau 39 : Vous gagnez 6 points d'armure et 45 HP et 6 points de dégât !\n\n" +
+                    "Niveau 40 : Vous gagnez 6 points d'armure et 45 HP et 6 points de dégât !")
             .setTimestamp()
         message.channel.send({ embed })
     }
