4b2a4a362f8114317dd67a00627359d5ddafb0f7
Red <b.darras854@laposte.net>
2019-11-04 16:19:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -448,10 +448,16 @@ bot.on('message', message => {
             .addField("Les armes :", "Vous trouverez ou fabriquerez parfois des armes plus ou moins rares, pouvant être des armes à une ou deux mains. Chaque arme améliore un ou des coups ou actions défensives !\n\n" +
                 "Vous saurez quelle arme permet d'améliorer quel coup ou action défensive en affichant les infos sur celle-ci en écrivant : `=[Nom de l'arme]`\n\n" +
                 "Lorsque vous créez votre personnage, vous aurez droit de choisir deux armes à une main ou une arme à deux mains dans la liste suivante :\n\n" +
-                "Les épées\nLes dagues\nLes glaives\nLes masses\nLes haches\nLes rapières\nLes cimeterres\nLes katanas\nLes boucliers\n\n" +
-                "Cependant, l'arme de base que vous aurez n'améliora pas de coups ou d'actions défensives pour le moment, seulement les armes que vous trouverez sur des monstres, achèterez ou fabriquerez !\n\n" +
-                "Les seuls exceptions sont : Les dagues et les boucliers, qui ne sont qu'à une main, et les glaives, qui sont toujours à deux mains !")
-            .setImage("https://www.nautiljon.com/images/univers/00/41/sword_art_online_aincrad_14.jpg")
+                "Les épées\n" +
+                "Les dagues\n" +
+                "Les glaives\n" +
+                "Les masses\n" +
+                "Les haches\n" +
+                "Les rapières\n" +
+                "Les cimeterres\n" +
+                "Les katanas\n" +
+                "Les boucliers\n\n" +
+                "Cependant, l'arme de base que vous aurez n'améliora pas de coups ou d'actions défensives pour le moment, seulement les armes que vous trouverez sur des monstres, achèterez ou fabriquerez !")
             .setTimestamp()
         message.channel.send({ embed })
     }
