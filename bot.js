209260156b74a492cad1d812eab868fd7e418d28
Red <b.darras854@laposte.net>
2020-01-04 23:34:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -37689,6 +37689,21 @@ bot.on('message', message => {
             .addBlankField(true)
             .addField(":crossed_swords: Si vous possédez un sabre :", ":crossed_swords: `=Coup dévastateur : [Vos dégâts]`")
             .addBlankField(true)
+            .addField(":crossed_swords:Pour la suite des coups et défenses des armes possibles :", "`=Coups et défenses des armes 2`")
+            .addBlankField(true)
+            .addField(":book: Pour comprendre à quoi sert chaques coups et défenses des armes :", ":book: `=Coups et défenses des armes descriptions`")
+            .setImage("https://i.pinimg.com/originals/18/fb/4b/18fb4b82ad92387d26413f1ef3518d96.png")
+            .setTimestamp()
+        message.channel.send({ embed })
+    }
+})
+
+bot.on('message', message => {
+    if (message.content === (prefix) + "Coups et défenses des armes") {
+        const embed = new Discord.RichEmbed()
+            .setColor(3447003)
+            .setAuthor(message.author.username, message.author.avatarURL)
+            .setFooter("『SAO Community [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
             .addField(":crossed_swords: Si vous possédez un katana :", ":crossed_swords: `=Coup précis : [Vos dégâts]`")
             .addBlankField(true)
             .addField(":crossed_swords: Si vous possédez un nodachi :", ":crossed_swords: `=Coup du cerisier : [Vos dégâts]`")
@@ -37706,8 +37721,6 @@ bot.on('message', message => {
             .addField(":crossed_swords: Si vous possédez un écu :", ":crossed_swords: `=Détournement : [Les dégâts adverses]`")
             .addBlankField(true)
             .addField(":crossed_swords: Si vous possédez une targe :", ":crossed_swords: `=Coup résonant`")
-            .addBlankField(true)
-            .addField(":book: Pour comprendre à quoi sert chaques coups et défenses des armes :", ":book: `=Coups et défenses des armes descriptions`")
             .setImage("https://i.pinimg.com/originals/18/fb/4b/18fb4b82ad92387d26413f1ef3518d96.png")
             .setTimestamp()
         message.channel.send({ embed })
@@ -37738,8 +37751,21 @@ bot.on('message', message => {
             .addBlankField(true)
             .addField("Le coup débilitant :", "C'est un coup moyennement puissant, permettant de diminuer les dégâts de la cible pendant un tour.")
             .addBlankField(true)
-            /*.addField("Le coup dévastateur :", "C'est un coup moyennement puissant, permettant parfois de paralyser la cible pendant un tour.")
+            .addField("Le coup dévastateur :", "C'est un coup moyennement puissant, permettant parfois de paralyser la cible pendant un tour.")
             .addBlankField(true)
+            .addField(":crossed_swords:Pour la suite des coups et défenses des armes possibles :", "`=Coups et défenses des armes descriptions 2`")
+            .setImage("https://i.pinimg.com/originals/18/fb/4b/18fb4b82ad92387d26413f1ef3518d96.png")
+            .setTimestamp()
+        message.channel.send({ embed })
+    }
+})
+
+bot.on('message', message => {
+    if (message.content === (prefix) + "Coups et défenses des armes descriptions") {
+        const embed = new Discord.RichEmbed()
+            .setColor(3447003)
+            .setAuthor(message.author.username, message.author.avatarURL)
+            .setFooter("『SAO Community [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
             .addField("Le coup précis :", "C'est un coup plutôt faible, permettant d'empêcher la cible d'attaquer pendant un tour.")
             .addBlankField(true)
             .addField("Le coup du cerisier :", "C'est un coup très puissant, permettant d'attaquer deux cibles adjacentes en même temps.")
@@ -37757,7 +37783,7 @@ bot.on('message', message => {
             .addField("Le détournement :", "C'est une défense, permettant parfois de renvoyer une partie des dégâts reçus vers l'un des coéquipiers de l'ennemi.")
             .addBlankField(true)
             .addField("Le coup résonant :", "C'est un coup permettant parfois d'empêcher la cible d'attaquer pendant un tour.")
-            .setImage("https://i.pinimg.com/originals/18/fb/4b/18fb4b82ad92387d26413f1ef3518d96.png")*/
+            .setImage("https://i.pinimg.com/originals/18/fb/4b/18fb4b82ad92387d26413f1ef3518d96.png")
             .setTimestamp()
         message.channel.send({ embed })
     }
